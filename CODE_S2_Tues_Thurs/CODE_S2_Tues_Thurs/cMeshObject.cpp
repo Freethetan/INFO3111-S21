1d6da1a2d5ac21d07dcb2b9d7a73d478e0154348
Michael Feeney <michael.feeney@badmonster.com>
2021-05-11 13:29:41 -0400

    Added the VAO manager and the cMeshObject (for a little later)

---
+++ CODE_S2_Tues_Thurs/CODE_S2_Tues_Thurs/cMeshObject.cpp
@@ -0,0 +1,16 @@
+#include "cMeshObject.h"
+
+cMeshObject::cMeshObject()
+{
+	this->meshName = "";
+	this->position = glm::vec3(0.0f, 0.0f, 0.0f);
+
+	this->wholeObjectColour = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);	// 0,0,0,1 (black)
+
+	this->orientation = glm::vec3(0.0f, 0.0f, 0.0f);		// Euler angles (x, y, and z)
+	
+	this->scale = 1.0f; // 1.0f
+
+	this->isWireframe = false;		// false
+
+}
---
+++ CODE_S2_Tues_Thurs/CODE_S2_Tues_Thurs/cMeshObject.h
@@ -0,0 +1,24 @@
+#ifndef _cMeshObject_HG_
+#define _cMeshObject_HG_
+
+#include "globalStuff.h"    
+
+#include <string>
+#include <glm/glm.hpp>
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+
+class cMeshObject
+{
+public:
+    cMeshObject();
+    ~cMeshObject() {};
+    std::string meshName;
+    glm::vec3 position;		// 0,0,0 (origin)
+    glm::vec4 wholeObjectColour;	// 0,0,0,1 (black)
+    glm::vec3 orientation;		// Euler angles (x, y, and z)
+    float scale;			// 1.0f
+    bool isWireframe;		// false
+};
+
+#endif // _cMeshObject_HG_
