eb9f783bae18285ed794a928c8a3ba6782ee1ac8
Michael Feeney <michael.feeney@badmonster.com>
2021-05-12 13:39:38 -0400

    Dot on bunny in shader

--- CODE_S1_Wed_Friday/CODE_S1_Wed_Friday/assets/shaders/myFragShad.glsl
+++ CODE_S1_Wed_Friday/CODE_S1_Wed_Friday/assets/shaders/myFragShad.glsl
@@ -1,9 +1,13 @@
-#version 110
+#version 420
 // myFragShad.glsl
 
-varying vec3 color;
+in vec3 colour;
+
+out vec4 outputColour;
 
 void main()
 {
-    gl_FragColor = vec4(color, 1.0);
+    //gl_FragColor = vec4(color, 1.0);
+	
+	outputColour = vec4(colour, 1.0f);
 };
--- CODE_S1_Wed_Friday/CODE_S1_Wed_Friday/assets/shaders/myVertShad.glsl
+++ CODE_S1_Wed_Friday/CODE_S1_Wed_Friday/assets/shaders/myVertShad.glsl
@@ -1,4 +1,4 @@
-#version 110
+#version 420
 // myVertShad.glsl
 
 uniform mat4 MVP;
@@ -6,10 +6,45 @@ uniform mat4 MVP;
 attribute vec3 vCol;
 attribute vec2 vPos;
 
-varying vec3 color;
+out vec3 colour;
 
 void main()
 {
-    gl_Position = MVP * vec4(vPos, 0.0, 1.0);
-    color = vCol;
+	float scale = 4.0f;
+	float xOffset = 0.0f;
+	float yOffset = -0.3f;	
+	float roation = 13.0f;
+	
+	vec2 thisVertex = vPos;
+
+	thisVertex.x *= scale;
+	thisVertex.y *= scale;
+
+	thisVertex.x += xOffset;
+	thisVertex.y += yOffset;		
+
+
+    //gl_Position = MVP * vec4(vPos, 0.0, 1.0);
+	gl_Position = MVP * vec4(thisVertex, 0.0f, 1.0f);
+	
+	// Now pick a colour
+	vec3 theColour = vCol;
+	theColour.r = 0.0f;
+	theColour.g = 0.0f;
+	theColour.b = 0.0f;
+	
+	// sqrt(X ^ 2 + y ^ 2)
+	float distance = sqrt( (thisVertex.x * thisVertex.x) + 
+						   (thisVertex.y * thisVertex.y) );
+	
+	if ( distance > 0.15f )
+	{
+		theColour.g = 1.0f;
+	}
+	else
+	{
+		theColour.r = 1.0f;
+	}
+	
+    colour = theColour;	// vCol;
 };
