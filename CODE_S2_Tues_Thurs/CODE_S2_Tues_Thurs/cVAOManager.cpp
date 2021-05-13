77623e725753cb94d05075a7844631439e7dda52
Michael Feeney <michael.feeney@badmonster.com>
2021-05-13 15:00:10 -0400

    Multiple models can be loaded, and some of the parameters can be read and used to display

--- CODE_S2_Tues_Thurs/CODE_S2_Tues_Thurs/cVAOManager.cpp
+++ CODE_S2_Tues_Thurs/CODE_S2_Tues_Thurs/cVAOManager.cpp
@@ -104,8 +104,8 @@ bool cVAOManager::LoadModelIntoVAO(
 
 	// Set the vertex attributes.
 
-	GLint vpos_location = glGetAttribLocation(shaderProgramID, "vPos");	// program
-	GLint vcol_location = glGetAttribLocation(shaderProgramID, "vCol");	// program;
+	GLint vpos_location = glGetAttribLocation(shaderProgramID, "thePositionXYZ");	// program
+	GLint vcol_location = glGetAttribLocation(shaderProgramID, "theVertexColour");	// program;
 
 	// Set the vertex attributes for this shader
 	glEnableVertexAttribArray(vpos_location);	// vPos
